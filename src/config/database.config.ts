b582c15b10b5e61b807ad107a584f566f3f02bb1
e-lglioui <elgliouiyoucode@gmail.com>
2025-01-04 14:54:46 +0100

    change database from mongodb to dynamodb

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -6,8 +6,8 @@ export default registerAs('database', () => {
   const client = new DynamoDBClient({
     region: process.env.AWS_REGION || 'eu-north-1',
     credentials: {
-      accessKeyId: process.env.AWS_ACCESS_KEY_ID || 'your-access-key-id',
-      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || 'your-secret-access-key',
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID ,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY ,
     },
   });
 
